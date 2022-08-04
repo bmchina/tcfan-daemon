8b8671a23b1528f2d51936dbca45334be883df66
bmchina <85761490+bmchina@users.noreply.github.com>
2022-08-04 20:41:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 rpi-rw
 sudo gcc tcfan_daemon.c  -o tcfan_daemon -lwiringPi -lpthread
 sudo chmod a+x ./tcfan_daemon
@@ -7,3 +6,4 @@ sudo cp -f ./tcfan.service /lib/systemd/system
 sudo cp -f ./tcfan_daemon /usr/local/sbin
 sudo cp -f ./start_tcfan.sh /etc
 sudo systemctl enable tcfan.service
+sudo systemctl start tcfan.service
