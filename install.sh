054590cf7a9e1dee82b7e94adf0c9c2e5d625f1d
bmchina <bmc@mbpos.com>
2022-08-05 01:16:20 +0800

    Modified

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo mount -o remount,rw /
 sudo mount -o remount,rw /boot
-#sudo gcc tcfan_daemon.c  -o tcfan_daemon -lwiringPi -lpthread
-#sudo chmod a+x ./tcfan_daemon
+sudo gcc tcfan_daemon.c  -o tcfan_daemon -lwiringPi -lpthread
+sudo chmod a+x ./tcfan_daemon
 sudo cp -f ./tcfan.service /lib/systemd/system
 sudo cp -f ./tcfan_daemon /usr/local/sbin
 sudo cp -f ./start_tcfan.sh /etc
