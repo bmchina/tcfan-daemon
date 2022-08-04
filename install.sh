e31785fcf0f6dc7f5865635e52a7b2891a4e56fb
bmchina <bmc@mbpos.com>
2022-08-05 01:07:05 +0800

    Update Install File.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rpi-rw
+sudo mount -o remount,rw /
+sudo mount -o remount,rw /boot
 #sudo gcc tcfan_daemon.c  -o tcfan_daemon -lwiringPi -lpthread
 #sudo chmod a+x ./tcfan_daemon
 sudo cp -f ./tcfan.service /lib/systemd/system
